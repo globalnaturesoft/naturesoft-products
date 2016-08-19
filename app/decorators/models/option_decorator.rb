089eafd19612ec6b8512b0865e64a477dab0b5bb
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-08-18 19:01:01 -0700

    fix setting products

---
+++ app/decorators/models/option_decorator.rb
@@ -0,0 +1,27 @@
+Naturesoft::Option.class_eval do
+  @products = {
+    "items_per_page" => 10,
+    "categories_items_per_page" => 200,
+    "manufacturers_items_per_page" => 10,
+    "thumbnails" => {
+      "0" => {
+        "code" => "big",
+        "width" => "500",
+        "height" => "500",
+        "scale" => "fit"
+      },
+      "1" => {
+        "code" => "medium",
+        "width" => "160",
+        "height" => "160",
+        "scale" => "fill"
+      },
+      "2" => {
+        "code" => "small",
+        "width" => "80",
+        "height" => "80",
+        "scale" => "fill"
+      }
+    },
+  }
+end
