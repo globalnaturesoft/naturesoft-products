ca92a2cf0a6c1136cbd30064cde73ddcb10c9a13
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-08-19 02:56:37 -0700

    update

---
+++ app/decorators/models/nsmodule_decorator.rb
@@ -0,0 +1,11 @@
+Naturesoft::Nsmodule.class_eval do
+  @products = {
+    "newest_products" => {
+      "label" => "Newest products",
+      "options" => {
+        "category_id" => nil,
+        "number_of_products" => 5
+      }
+    }
+  }
+end
