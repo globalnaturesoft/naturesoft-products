9484fe32da0b5e17d060c327db44bd8eaef4aecf
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-09-25 20:25:40 -0700

    fix available for menus engine

--- app/decorators/models/menu_decorator.rb
+++ app/decorators/models/menu_decorator.rb
@@ -1,12 +1,14 @@
-Naturesoft::Menus::Menu.class_eval do
-  @products = {
-    "categories_products" => {
-      "label" => "Categories / Products List",
-      "controller" => "/naturesoft/products/categories",
-      "action" => "list",
-      "params" => {
-        "id" => nil
+if Naturesoft::Core.available?("menus")
+  Naturesoft::Menus::Menu.class_eval do
+    @products = {
+      "categories_products" => {
+        "label" => "Categories / Products List",
+        "controller" => "/naturesoft/products/categories",
+        "action" => "list",
+        "params" => {
+          "id" => nil
+        }
       }
     }
-  }
+  end
 end
